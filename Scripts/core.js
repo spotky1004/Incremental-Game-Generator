9de5e97c59b8619cb7b30886a67d759f50c41740
Spotky1004 <spotky1004@gmail.com>
2021-03-18 02:13:58 +0900

    almost dome first content

--- Scripts/core.js
+++ Scripts/core.js
@@ -6,6 +6,8 @@ function generateGame() {
         saveData: {}, // saveData
     
         contentUnlocked: 0,
+        totalContents: 0,
+        layers: {},
         timeSpent: 0, // total time spent on this game
         deltaTimeSpent: 0, // time spent without any progress
     }
@@ -13,14 +15,18 @@ function generateGame() {
     saveData.layers = {};
     for (const layer in generateConfig.layers) {
         generateConfig.layers[layer] = new Layer(generateConfig.layers[layer]);
+        sessionData.totalContents += generateConfig.layers[layer].upgrade.length;
+        sessionData.layers[layer] = {};
+        sessionData.layers[layer].upgrade = 0;
 
+        saveData.layerUnlocked = 0;
         saveData.layers[layer] = {
             resource: new D(0),
             upgradeBought: []
         };
     }
 
-    simulateLoop = setInterval(function(){if (!windowBlured) simulateGameTick()}, 1);
+    simulateLoop = setInterval(function(){if (!windowBlured) simulateGameTick()}, 10);
 }
 
 function simulateGameTick() {
@@ -28,36 +34,52 @@ function simulateGameTick() {
     sessionData.deltaTimeSpent += generateConfig.speed;
 
     let layerResMult = {};
-    let unlockOrderBool = true;
     for (const layer in generateConfig.layers) {
         layerResMult[layer] = new D(1);
-        for (let i = 0, l = generateConfig.layers[layer].upgrade.length; i < l; i++) {
-            if (typeof generateConfig.layers[layer].upgrade[i].cost === "undefined" && unlockOrderBool) {
-                unlockOrderBool = false;
-                if (sessionData.deltaTimeSpent/1000 > getDifficulty()*(0.5+((generateConfig.seed+i**5)%8)/4)) {
-                    sessionData.deltaTimeSpent = 0;
-                    saveData.layers[layer].upgradeBought.push(i);
-                    generateConfig.layers[layer].upgrade[i].cost = new D(saveData.layers[layer].resource);
-                    if (generateConfig.clearify) generateConfig.layers[layer].upgrade[i].cost = Spdl.clearify(generateConfig.layers[layer].upgrade[i].cost);
-                    saveData.layers[layer].resource = saveData.layers[layer].resource.sub(generateConfig.layers[layer].upgrade[i].cost);
-                    sessionData.contentUnlocked++;
-                }
+        const thisLayer = generateConfig.layers[layer];
+        const thisSave = saveData.layers[layer];
+
+        for (let i = 0, l = thisLayer.upgrade.length; i < l; i++) {
+            if (!thisSave.upgradeBought.includes(i) && typeof thisLayer.upgrade[i].cost !== "undefined" && thisSave.resource.gt(thisLayer.upgrade[i].cost)) {
+                thisSave.resource = thisSave.resource.sub(thisLayer.upgrade[i].cost);
+                thisSave.upgradeBought.push(i);
             }
 
-            if (saveData.layers[layer].upgradeBought.includes(i)) {
-                layerResMult[layer] = layerResMult[layer][generateConfig.layers[layer].upgrade[i].boostType](generateConfig.layers[layer].upgrade[i].boost);
+            if (thisSave.upgradeBought.includes(i)) {
+                layerResMult[layer] = layerResMult[layer][thisLayer.upgrade[i].boostType](thisLayer.upgrade[i].boost());
             }
         }
-        saveData.layers[layer].resource = saveData.layers[layer].resource.add(generateConfig.layers[layer].baseResourceGenerate.mul(layerResMult[layer]).mul(generateConfig.speed/1000));
+
+        const upgradeIdx = sessionData.layers[layer].upgrade;
+        if (
+            thisLayer.upgrade.length > sessionData.layers[layer].upgrade &&
+            sessionData.deltaTimeSpent/1000 > thisLayer.getDifficulty()*(0.4+((generateConfig.seed+upgradeIdx**5)%24)/4)
+        ) {
+            thisSave.upgradeBought.push(upgradeIdx);
+            thisLayer.upgrade[upgradeIdx].cost = new D(thisSave.resource);
+            if (generateConfig.clearify) thisLayer.upgrade[upgradeIdx].cost = Spdl.clearify(thisLayer.upgrade[upgradeIdx].cost);
+            thisSave.resource = thisSave.resource.sub(thisLayer.upgrade[upgradeIdx].cost);
+
+            sessionData.deltaTimeSpent = 0;
+            sessionData.contentUnlocked++;
+            sessionData.layers[layer].upgrade++;
+        }
+
+        thisSave.resource = thisSave.resource.add(thisLayer.baseResourceGenerate.mul(layerResMult[layer]).mul(generateConfig.speed/1000));
     }
     displayGenerated();
+
+    if (sessionData.totalContents == sessionData.contentUnlocked) {
+        clearInterval(simulateLoop);
+        document.getElementById("generateDisplay").innerHTML = "done!<br><br>turn into a html file isn't supported yet...";
+    }
 }
 
 function displayGenerated() {
     let displayTxt = "";
     for (const layer in generateConfig.layers) {
         displayTxt += `You have ${notation(saveData.layers[layer].resource)} ${generateConfig.layers[layer].resourceName} (${generateConfig.layers[layer].shortResourceName})<br>`;
-        displayTxt += `${sessionData.contentUnlocked}/${generateConfig.layers[layer].upgrade.length} Upgrades generated so far...<br>`;
+        displayTxt += `${sessionData.layers[layer].upgrade}/${generateConfig.layers[layer].upgrade.length} Upgrades generated so far...<br>`;
         displayTxt += "<br>";
     }
     document.getElementById("generateDisplay").innerHTML = displayTxt;
--- Scripts/data.js
+++ Scripts/data.js
@@ -3,19 +3,22 @@ let generateConfig = {
     layers: {
         basic: {
             name: "Basic",
-            upgradeCount: 100,
+            upgradeCount: 300,
             baseResourceGenerate: new D(1)
         },
-        /*advenced: {
-            unlockesAt: () => {saveData.layers.basic.upgrades.length >= 50},
-            upgrades: 10
-        }*/
+        advenced: {
+            unlock: {
+                when: () => {saveData.layers.basic.upgradeBought.length >= 50},
+            },
+            name: "Advanced",
+            upgrades: 30
+        }
     }, // data of layers
     contents: 0, //
     difficulty: 3, // this is affect to progress speed (highter -> longer)
-    deltaDifficulty: 0.2, // difficulty increases over progress (def. difficulty += deltaDifficulty)
+    deltaDifficulty: 0.1, // difficulty increases over progress (def. difficulty += deltaDifficulty)
     seed: -1, // seed, -1 to random (0 ~ 1e10)
-    speed: 100, // generate speed in ms/loop, increasing this will cause quality down
+    speed: 300, // generate speed in ms/loop, increasing this will cause quality down
     clearify: true, // make generated values clear. ex) 1.2345e8324 -> 1.2e8324
 }
 
@@ -24,29 +27,56 @@ let gameData = {};
 let layerIndex = 0;
 class Layer {
     constructor(attr={}) {
-        this.name = attr.name || "Basic";
-        this.index = typeof attr.index !== "undefined" ? attr.index : layerIndex;
-        layerIndex++;
-        this.upgrade = [];
-        for (let i = 0; i < attr.upgradeCount; i++) this.upgrade.push(this.generateUpgrade());
-        this.diffMult = typeof attr.diffMult !== "undefined" ? attr.diffMult : 1;
-        this.baseResourceGenerate = attr.baseResourceGenerate || new D(0);
+        this.name = attr.name || "Basic"; // name of the layer
         this.resourceName = this.resourceName || attr.name + " Point";
         this.shortResourceName = this.resourceName.includes(" ") ? this.resourceName.split(" ").reduce((a, b) => a+b[0], "").toUpperCase() : this.resourceName;
+
+        this.index = typeof attr.index !== "undefined" ? attr.index : layerIndex; // index of layer (it's better to don't change this)
+        layerIndex++;
+
+        this.upgrade = []; // define upgrade
+        for (let i = 0; i < attr.upgradeCount; i++) this.upgrade.push(new Upgrade(this.index, this.upgrade.length)); // fill upgrades
+
+        this.unlock = new LayerUnlock(attr.unlock || {}); // see class LayerUnlock
+        
+        this.difficultyMultiply = typeof attr.difficultyMultiply !== "undefined" ? attr.difficultyMultiply : 1;
+        this.baseResourceGenerate = attr.baseResourceGenerate || new D(0);
     }
 
-    generateUpgrade() {
-        const index = this.upgrade.length;
-        const innerSeed = generateConfig.seed+index**2;
-        const boost = new D(innerSeed%6+4).pow(Math.pow(index+1, 0.4)).floor(0);
-        return {
-            index: index+1,
-            boost: boost,
-            desc: `Multiply ${generateConfig.layers.shortResourceName} gain by ${notation(boost, 2, 0)}`,
-            cost: undefined,
-            boostType: "mul",
-            boostTo: "basic"
-        };
+    
+
+    getDifficulty() {
+        return (getDifficulty() || generateConfig.difficulty)*this.difficultyMultiply;
     }
 }
 
+class Upgrade {
+    constructor(layerIdx, upgradeIdx) {
+        this.index = upgradeIdx;
+
+        const innerSeed = generateConfig.seed+this.index**2;
+        const upgradeTemple = upgradeVariety[innerSeed%(Math.min(upgradeVariety.length+3+this.index, upgradeVariety.length))];
+        
+        this.boost = upgradeTemple.boost(innerSeed, this.index);
+        this.desc = upgradeTemple.desc;
+        this.cost = undefined;
+        this.boostType = upgradeTemple.boostType;
+        this.boostTo = Object.keys(generateConfig.layers)[layerIdx ? innerSeed%layerIdx : 0];
+    }
+}
+
+class LayerUnlock {
+    constructor(attr={}) {
+        this.when = attr.when || function(){return 1;}; // bool, when to unlock
+        this.whenUnlocked = attr.whenUnlocked || function(){return 0;}; // do something when unlocked
+    }
+}
+
+const upgradeVariety = [
+    // 0
+    {
+        desc: `Multiply $Rescource gain by x$Boost`,
+        boost: function(seed, index) {return new Function(`return new D(${seed}%6+4).pow(Math.pow(${index}+1, 0.4)).floor(0)`)},
+        boostType: "mul"
+    }
+];
